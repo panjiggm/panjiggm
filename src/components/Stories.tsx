1d1595d3e75144e51c0747efe4efe10310d92009
Panji Gumelar <panjiggm@gmail.com>
2022-10-12 19:51:08 +0700

    hide scrollbar

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -3,7 +3,7 @@ import Story from "./Story";
 
 const Stories = () => {
   return (
-    <div className="flex space-x-5 overflow-x-scroll max-w-xl border-gray-200 scroll-smooth">
+    <div className="flex space-x-5 overflow-x-scroll max-w-xl border-gray-200 scroll-smooth scrollbar-hide">
       {stories.map((story) => {
         return (
           <Story
